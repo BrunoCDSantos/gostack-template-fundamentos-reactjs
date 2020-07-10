be16201f686864746cc84b4e0ed517f93f89cee9
Brunokkkk <bruno.coelho.santos17@gmail.com>
2020-07-10 13:51:33 -0300

    fazer modo dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import light from './styles/themes/light';
+import dark from './styles/themes/darks';
+import usePersistedState from './utils/usePersistedState';
+
 import Routes from './routes';
+import SwitchButton from './components/SwitchButton';
 
 import GlobalStyle from './styles/global';
 
-const App: React.FC = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
-);
+const App: React.FC = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>(
+    '@financialapp:theme',
+    dark,
+  );
+
+  function toggleTheme(name: string): void {
+    setTheme(name === 'light' ? light : dark);
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </ThemeProvider>
+  );
+};
 
 export default App;
